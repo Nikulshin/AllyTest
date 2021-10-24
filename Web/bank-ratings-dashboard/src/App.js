3a27b4e02de7bcb0fd0f7f0f045c7bbb20505db5
Aleksey <aleksey.nikulshin@gmail.com>
2021-10-23 22:20:53 -0400

    Page with inputs created

--- Web/bank-ratings-dashboard/src/App.js
+++ Web/bank-ratings-dashboard/src/App.js
@@ -1,21 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import {InputsComponent} from "./components/Inputs";
+import {Container} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <InputsComponent/>
-        </div>
-      </header>
-    </div>
-  );
+
+    const requestListener = () => {
+        alert('Done!');
+    };
+    const invokeCalc = (date) => {
+        alert('before!');
+      let req = new XMLHttpRequest();
+      req.addEventListener("load", requestListener);
+      req.open("POST", "https://localhost:5001/TradeLimits?valuationDate=" + date);
+      req.send();
+    };
+
+    return (
+      <Container className="p-3" >
+          <h1 className="header">Welcome To Bank Ratings Calculator</h1>
+          <div className="Inputs">
+              <InputsComponent calculate={invokeCalc}/>
+          </div>
+      </Container>
+
+    );
 }
 
 export default App;
--- Web/bank-ratings-dashboard/src/components/Inputs.jsx
+++ Web/bank-ratings-dashboard/src/components/Inputs.jsx
@@ -1,16 +1,27 @@
-import {FormControl, InputGroup} from "react-bootstrap";
+//import {FormControl, InputGroup, Button} from "react-bootstrap";
+import React from "react";
+import DatePicker from "react-datepicker";
 
-export function InputsComponent (props) {
-    return (
-        <>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                <FormControl
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                />
-            </InputGroup>
-        </>
-    );
+export class InputsComponent extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {valuationDate: new Date()};
+        this.calculate = this.calculate.bind(this);
+    }
+
+    calculate() {
+        this.props.calculate(this.state.valuationDate);
+    }
+
+
+    render() {
+        return (
+            <>
+                <DatePicker selected={this.state.valuationDate} onChange={(date) => this.setState({valuationDate: date})}/>
+                <button onClick={this.calculate}>Calculate</button>
+                {/*<div>{this.state.valuationDate.toDateString()}</div>*/}
+            </>
+        );
+    }
 }
